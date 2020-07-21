ce22934cfba3fabd16ce9a0b9726536fe883d216
Phil Rzewski <phil@brimsecurity.com>
2020-07-21 15:48:14 -0700

    Debug

--- smoketest.sh
+++ smoketest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Install latest binary Zeek (as described below https://zeek.org/get-zeek/)
 echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ /' | tee /etc/apt/sources.list.d/security:zeek.list
@@ -12,7 +12,7 @@ export PATH="/opt/zeek/bin:$PATH"
 zkg autoconfig
 zkg install --force geoip-conn --version "$PACKAGE_SHA"
 find /opt/zeek/share/zeek/site
-echo "@load ./geoip-conn/zeek" >> /opt/zeek/share/zeek/site/local.zeek
+echo '@load packages' | tee -a /opt/zeek/share/zeek/site/local.zeek
 
 # Do a lookup of an IP that's known to have a stable location.
 zeek -e "print lookup_location(199.83.220.115);" local
