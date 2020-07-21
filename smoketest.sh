77b40c50330c12fb13dad8b80141e7b8ca484bcb
Phil Rzewski <phil@brimsecurity.com>
2020-07-21 16:01:38 -0700

    Compile Zeek

--- smoketest.sh
+++ smoketest.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -x
 
-apt-get update
-apt-get install libmaxminddb-dev
-
-# Install latest binary Zeek (as described below https://zeek.org/get-zeek/)
-echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ /' | tee /etc/apt/sources.list.d/security:zeek.list
-curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_18.04/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/security:zeek.gpg > /dev/null
-apt update
-apt install zeek
+# Alas, we must compile Zeek because I've found the binary distributions are
+# not compiled with libmaxminddba.
+sudo apt-get update
+sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmaxminddb-dev
+git clone --recursive https://github.com/zeek/zeek
+cd zeek
+./configure --prefix=/usr/local/zeek
+make -j$(nproc)
+sudo make -j$(nproc) install
 
 # Add Zeek Package Manager and current revision of the geoip-conn package
-pip install zkg
-export PATH="/opt/zeek/bin:$PATH"
-zkg autoconfig
-zkg install --force geoip-conn --version "$PACKAGE_SHA"
+sudo pip install zkg
+PATH="/usr/local/zeek/bin:$PATH" sudo zkg autoconfig
+sudo zkg install --force geoip-conn --version "$PACKAGE_SHA"
 find /opt/zeek/share/zeek/site
-echo '@load packages' | tee -a /opt/zeek/share/zeek/site/local.zeek
+echo '@load packages' | sudo tee -a /opt/zeek/share/zeek/site/local.zeek
 
 # Do a lookup of an IP that's known to have a stable location.
-zeek -e "print lookup_location(199.83.220.115);" local
+sudo /usr/local/zeek/bin/zeek -e "print lookup_location(199.83.220.115);" local
