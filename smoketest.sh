af5c7f5fb150e62375ca7f695f0d5f2e344128be
Phil Rzewski <phil@brimsecurity.com>
2020-07-21 17:58:00 -0700

    Passing version

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Alas, we must compile Zeek because I've found the binary distributions are
-# not compiled with libmaxminddba.
+# not compiled with libmaxminddb.
 apt-get update
 apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmaxminddb-dev
 git clone --recursive https://github.com/zeek/zeek zeek-src
@@ -15,8 +15,7 @@ pip install zkg
 export PATH="/usr/local/zeek/bin:$PATH"
 zkg autoconfig
 zkg install --force geoip-conn --version "$PACKAGE_SHA"
-find /usr/local/zeek/share/zeek/site
 echo '@load packages' | tee -a /usr/local/zeek/share/zeek/site/local.zeek
 
 # Do a lookup of an IP that's known to have a stable location.
-zeek -e "print lookup_location(199.83.220.115);" local | grep "Somewhere Else"
+zeek -e "print lookup_location(199.83.220.115);" local | grep "San Francisco"
